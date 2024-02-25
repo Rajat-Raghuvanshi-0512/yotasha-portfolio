9e7be2d692b9d3cc4ec98ec1fd8185126520489c
Rajat Raghuvanshi <--global>
2024-02-25 16:03:36 +0530

    Added mobile sidebar

---
+++ components/Drawer.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Drawer({ children, isOpen, setIsOpen }) {
+  return (
+    <div
+      className={
+        " fixed overflow-hidden z-10 bg-opacity-25 top-0 right-0 shadow drop-shadow-sm h-screen transform ease-in-out " +
+        (isOpen
+          ? " transition-opacity opacity-100 duration-500 translate-x-0  "
+          : " transition-all delay-500 opacity-0 translate-x-full  ")
+      }
+    >
+      <section
+        className={
+          " max-w-lg right-0 absolute w-[80%] bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
+          (isOpen ? " translate-x-0 " : " translate-x-full ")
+        }
+      >
+        <article className="relative w-full pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
+          <header className="p-4 font-bold text-3xl text-orange-dark flex gap-5">
+            <Image src="/logo.webp" alt="logo" width={20} height={20} />
+            <p>Yotashaw</p>
+          </header>
+          {children}
+        </article>
+      </section>
+      <section
+        className=" w-screen h-full cursor-pointer "
+        onClick={() => {
+          setIsOpen(false);
+        }}
+      ></section>
+    </div>
+  );
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,10 +2,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import Drawer from "./Drawer";
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="border-b-orange-base border border-opacity-40 py-2 px-5 md:py-4  md:px-14 justify-between flex fixed w-full left-0 top-0 bg-white shadow-lg shadow-orange-base/10 z-10">
       <Link href="/">
@@ -17,6 +19,70 @@ const Navbar = () => {
           className="w-10 h-10 object-contain md:w-14 md:h-14"
         />
       </Link>
+      <Image
+        src="/burger-menu.png"
+        alt="menu"
+        width={20}
+        height={20}
+        className="md:hidden object-contain"
+        onClick={() => setIsOpen(true)}
+      />
+      <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
+        <ul className="gap-5 font-medium text-center text-orange-dark text-xl items-center h-full justify-center md:hidden flex flex-col">
+          <li>
+            <Link
+              className={
+                pathname === "/"
+                  ? "underline drop-shadow-md text-orange-base shadow-orange-dark underline-offset-8"
+                  : ""
+              }
+              href="/"
+              onClick={() => setIsOpen(false)}
+            >
+              home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={
+                pathname === "/about"
+                  ? "underline drop-shadow-md shadow-orange-dark text-orange-base underline-offset-8"
+                  : ""
+              }
+              href="/about"
+              onClick={() => setIsOpen(false)}
+            >
+              about
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={
+                pathname === "/resume"
+                  ? "underline drop-shadow-md shadow-orange-dark text-orange-base underline-offset-8"
+                  : ""
+              }
+              href="/resume"
+              onClick={() => setIsOpen(false)}
+            >
+              resume
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={
+                pathname === "/contact"
+                  ? "underline drop-shadow-md shadow-orange-dark text-orange-base underline-offset-8"
+                  : ""
+              }
+              href="/contact"
+              onClick={() => setIsOpen(false)}
+            >
+              contact
+            </Link>
+          </li>
+        </ul>
+      </Drawer>
       <ul className="gap-5 md:gap-7 lg:gap-10 font-medium text-orange-dark items-center hidden md:flex 2xl:text-xl">
         <li>
           <Link
@@ -26,6 +92,7 @@ const Navbar = () => {
                 : ""
             }
             href="/"
+            onClick={() => setIsOpen(false)}
           >
             home
           </Link>
@@ -38,6 +105,7 @@ const Navbar = () => {
                 : ""
             }
             href="/about"
+            onClick={() => setIsOpen(false)}
           >
             about
           </Link>
